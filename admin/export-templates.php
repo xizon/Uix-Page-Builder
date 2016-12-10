9a4ae70b665f629560491b40540d204f2144b912
Chuckie Chang <546007561@qq.com>
2016-12-10 21:49:29 +0800

    Update

--- admin/export-templates.php
+++ admin/export-templates.php
@@ -6,6 +6,11 @@
  */
 require_once( dirname( dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) ) . '/wp-load.php' );
 
+if ( !current_user_can('export') ) {
+    wp_die( __( 'Sorry, you are not allowed to export the content of this site.' ) );	
+}
+
+
 //Export a new templates xml file for developer	
 header( 'Content-Description: File Transfer' );
 header( 'Content-Disposition: attachment; filename=uix-pagebuilder-templates.xml' );
