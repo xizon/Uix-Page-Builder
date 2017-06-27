bf6bf617997a1fda98b552be690f1f1bb042e9df
Chuckie Chang <546007561@qq.com>
2017-06-27 15:35:34 +0800

    Update

--- admin/classes/section-shortcodes/class-section-googlemap.php
+++ admin/classes/section-shortcodes/class-section-googlemap.php
@@ -60,7 +60,7 @@ if ( !class_exists( 'UixPB_Map' ) ) {
 		function enqueue_map_display_script() {
 			
 			$script = '
-				!function(a){var b={};a.UixPBTmpl=function a(c,d){var e=/\W/.test(c)?new Function("obj","var p=[],print=function(){p.push.apply(p,arguments);};with(obj){p.push(\'"+c.replace(/[\r\t\n]/g," ").split("<%").join("\t").replace(/((^|%>)[^\t]*)\'/g,"$1\r").replace(/\t=(.*?)%>/g,"\',$1,\'").split("\t").join("\');").split("%>").join("p.push(\'").split("\r").join("\\\'")+"\');}return p.join(\'\');"):b[c]=b[c]||a(null===document.getElementById(c)?\'<div data-tmpl="0"></div>\'+c:document.getElementById(c).innerHTML);return d?e(d):e},a.fn.UixPBTmpl=function(b,c){return this.each(function(){var d=a.UixPBTmpl(b,c);a(this).html(d).promise().done(function(){a("#uix-page-builder-themepreview").load(function(){a(this).contents().text().search("[uix_pb_sections]")>0&&a(document).UixPBRenderWPShortcode({postID:uix_page_builder_layoutdata.send_string_postid})})})})}}(jQuery);
+				!function(a){var b={};a.UixPBTmpl=function a(c,d){var e=/\W/.test(c)?new Function("obj","var p=[],print=function(){p.push.apply(p,arguments);};with(obj){p.push(\'"+c.replace(/[\r\t\n]/g," ").split("<%").join("\t").replace(/((^|%>)[^\t]*)\'/g,"$1\r").replace(/\t=(.*?)%>/g,"\',$1,\'").split("\t").join("\');").split("%>").join("p.push(\'").split("\r").join("\\\'")+"\');}return p.join(\'\');"):b[c]=b[c]||a(null===document.getElementById(c)?\'<div data-tmpl="0"></div>\'+c:document.getElementById(c).innerHTML);return d?e(d):e},a.fn.UixPBTmpl=function(b,c){return this.each(function(){var d=a.UixPBTmpl(b,c);a(this).html(d)})}}(jQuery);
 
 				( function( $ ) {
 				"use strict";
